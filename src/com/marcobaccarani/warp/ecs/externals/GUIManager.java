8698054de8ac010fa5f250282ba52207ebd0ea36
Marco Baccarani <bacca87@gmail.com>
2014-04-22 15:30:48 +0200

    Added the convenience method setViewport(ScalingViewport viewport) in GUIManager class

--- src/com/marcobaccarani/warp/ecs/externals/GUIManager.java
+++ src/com/marcobaccarani/warp/ecs/externals/GUIManager.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GUIManager implements Disposable {
 	private Stage stage;
@@ -23,6 +25,10 @@ public class GUIManager implements Disposable {
 	
 	public void resize(int width, int height) {
 	    stage.getViewport().update(width, height, true);
+	}
+	
+	public void setViewport(ScalingViewport viewport) {
+		stage.setViewport(viewport);
 	}
 	
 	public void addActor(Actor actor) {
