e8ee47aed0c88c83af2f9ec06cb04a769b774744
Marco Baccarani <bacca87@gmail.com>
2014-05-05 23:23:27 +0200

    Updated libgdx to the last nightly release. GUI Manager updated with attatch and detach inputs methods

--- src/com/marcobaccarani/warp/ecs/externals/GUIManager.java
+++ src/com/marcobaccarani/warp/ecs/externals/GUIManager.java
@@ -5,14 +5,25 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.ScalingViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GUIManager implements Disposable {
 	private Stage stage;
-	
-	public GUIManager(InputMultiplexer input) {
+	private InputMultiplexer input;
+
+	public GUIManager() {
 		stage = new Stage();
+	}
+	
+	public void setInputMultiplexer(InputMultiplexer input) {
+		this.input = input;
+	}
+	
+	public void attachInputs() {
 		input.addProcessor(0, stage);
+	}
+	
+	public void detachInputs() {
+		input.removeProcessor(stage);
 	}
 	
 	public void update(float delta) {
